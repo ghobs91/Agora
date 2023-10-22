c59a5a2837b6e7f24ce3491c7d1143145d967524
Lim Chee Aun <cheeaun@gmail.com>
2023-10-22 19:26:41 +0800

    Debounce checks, less noisy

--- src/components/timeline.jsx
+++ src/components/timeline.jsx
@@ -239,6 +239,10 @@ function Timeline({
     },
     [id, loadItems, checkForUpdates, snapStates.settings.autoRefresh],
   );
+  const debouncedLoadOrCheckUpdates = useDebouncedCallback(
+    loadOrCheckUpdates,
+    3000,
+  );
 
   const lastHiddenTime = useRef();
   usePageVisibility(
@@ -246,12 +250,14 @@ function Timeline({
       if (visible) {
         const timeDiff = Date.now() - lastHiddenTime.current;
         if (!lastHiddenTime.current || timeDiff > 1000 * 60) {
-          loadOrCheckUpdates({
+          // 1 minute
+          debouncedLoadOrCheckUpdates({
             disableIdleCheck: true,
           });
         }
       } else {
         lastHiddenTime.current = Date.now();
+        debouncedLoadOrCheckUpdates.cancel();
       }
       setVisible(visible);
     },
